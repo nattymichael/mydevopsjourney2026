e326ef24e316f892bfb1a772f9d3e5a8544fcbc6
Ubuntu <ubuntu@ip-172-31-71-169.ec2.internal>
2026-04-27 18:18:43 +0000

    Added devops project 3 (full-stack app with EC2 + Nginx + MongoDB Atlas)

---
+++ devops-project3/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("DevOps Project Running 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
